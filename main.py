345da80317e38322f5c3b2782c83fbed817455bc
empty2145 <tasybaevtamir@gmail.com>
2026-02-06 19:01:06 +0500

    files

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+MAX_LINES = 3
+
+
 def deposit():
     while True:
         amount = input("What amount would you like to deposit? $")
         if amount.isdigit():
             amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than zero. Please try again.")
+        else:
+            print("Amount must be a number. Please try again.")
+    return amount
+
+def get_number_of_lines():
+        while True:
+            lines = input("Enter the number of lines to bet on (1-A")
+            if amount.isdigit():
+                amount = int(amount)
             if amount > 0:
                 break
             else:

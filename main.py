90bdaed6b4763fd14ae5c64b54e693fb18a284be
empty2145 <tasybaevtamir@gmail.com>
2026-02-10 21:07:55 +0500

    function main() to bet money

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 MAX_LINES = 3
-
+MAX_BET = 100
+MIN_BET = 1
 
 def deposit():
     while True:
@@ -14,19 +15,45 @@ def deposit():
             print("Amount must be a number. Please try again.")
     return amount
 
+
 def get_number_of_lines():
-        while True:
-            lines = input("Enter the number of lines to bet on (1-A")
-            if amount.isdigit():
-                amount = int(amount)
-            if amount > 0:
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
                 break
             else:
-                print("Amount must be greater than zero. Please try again.")
+                print("Enter a valid number. Please try again.")
+        else:
+            print("Amount must be a number. Please try again.")
+    return lines
+
+def get_bet():
+    while True:
+        amount = input("What amount would you like to bet on each line?$")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}. Please try again.")
         else:
             print("Amount must be a number. Please try again.")
     return amount
+
+
 def main():
     balance = deposit()
+    lines = get_number_of_lines()
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You don't have enough money to bet on this line, your current balance is ${balance}")
+        else:
+            break
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to ${total_bet}")
 
 main()
